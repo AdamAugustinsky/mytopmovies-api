6305af6f355a596a78c8635e91a890d116f16496
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-09-09 21:27:28 -0300

    refactor(moviesServiceTest): use different test pattern

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,19 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
   let service: MoviesService;
-
-  const mockMovie: Movie = new Movie();
-  mockMovie.id = 0;
-  mockMovie.title = "test title";
-  mockMovie.description = "test description";
-  mockMovie.director = "test director";
-  mockMovie.imageUrl = "http://en.wikipedia.org/wiki/File:Ada_lovelace.jpg";
-  mockMovie.releaseDate = new Date('2001-01-01');
-  mockMovie.rating = 5;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -22,9 +14,20 @@ describe('MoviesService', () => {
         {
           provide: getRepositoryToken(Movie),
           useValue: {
-            create: jest.fn().mockResolvedValueOnce(mockMovie),
-            save: jest.fn().mockResolvedValueOnce(mockMovie),
-            find: jest.fn().mockResolvedValueOnce([mockMovie]),
+            create: jest.fn((createMovieDto: CreateMovieDto) => {
+              const movie: Movie = new Movie();
+
+              movie.id = 1;
+              movie.title = createMovieDto.title;
+              movie.description = createMovieDto.description;
+              movie.director = createMovieDto.director;
+              movie.rating = createMovieDto.rating;
+              movie.releaseDate = createMovieDto.releaseDate;
+              movie.imageUrl = createMovieDto.imageUrl;
+
+              return Promise.resolve(movie);
+            }),
+            save: jest.fn((movie: Movie) => Promise.resolve(movie)),
           }
         }
       ],
@@ -37,16 +40,21 @@ describe('MoviesService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should be able to create a movie', async () => {
-    const createdMovie = await service.create({
-      title: "teste",
-      description: "test description",
-      director: "test director",
-      releaseDate: new Date(),
-      rating: 0,
-      imageUrl: "http://en.wikipedia.org/wiki/File:Ada_lovelace.jpg"
-    })
+  describe("createMovie", () => {
+    it('should be able to create a movie', async () => {
+      const createMovieDto = {
+        title: "test title",
+        description: "test description",
+        director: "test director",
+        releaseDate: new Date('2001-01-01'),
+        rating: 1,
+        imageUrl: "test image url",
+      };
 
-    expect(createdMovie).toBeInstanceOf(Movie);
-  });
+      await expect(service.create(createMovieDto)).resolves.toEqual({
+        id: 1,
+        ...createMovieDto
+      });
+    });
+  })
 });
