8e7af67033fa9cab710e5d95499118df7690ccd9
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-09-09 21:28:30 -0300

    refactor(moviesControllerTest): use different test pattern

--- src/movies/movies.controller.spec.ts
+++ src/movies/movies.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
@@ -24,9 +25,20 @@ describe('MoviesController', () => {
         {
           provide: getRepositoryToken(Movie),
           useValue: {
-            create: jest.fn().mockResolvedValueOnce(mockMovie),
-            save: jest.fn().mockResolvedValueOnce(mockMovie),
-            find: jest.fn().mockResolvedValueOnce([mockMovie]),
+            create: jest.fn((createMovieDto: CreateMovieDto) => {
+              const movie: Movie = new Movie();
+
+              movie.id = 1;
+              movie.title = createMovieDto.title;
+              movie.description = createMovieDto.description;
+              movie.director = createMovieDto.director;
+              movie.rating = createMovieDto.rating;
+              movie.releaseDate = createMovieDto.releaseDate;
+              movie.imageUrl = createMovieDto.imageUrl;
+
+              return Promise.resolve(movie);
+            }),
+            save: jest.fn((movie: Movie) => Promise.resolve(movie)),
           }
         }
       ],
@@ -38,4 +50,22 @@ describe('MoviesController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe("createMovie", () => {
+    it('should be able to create a movie', async () => {
+      const createMovieDto = {
+        title: "test title",
+        description: "test description",
+        director: "test director",
+        releaseDate: new Date('2001-01-01'),
+        rating: 1,
+        imageUrl: "test image url",
+      };
+
+      await expect(controller.create(createMovieDto)).resolves.toEqual({
+        id: 1,
+        ...createMovieDto
+      });
+    });
+  })
 });
