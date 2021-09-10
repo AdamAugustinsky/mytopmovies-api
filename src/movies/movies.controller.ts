41c2616c63f494385e15cc9e9cb6231c843bff50
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-09-10 11:05:49 -0300

    refactor(movies.controller): id to number

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -20,17 +20,17 @@ export class MoviesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: number) {
     return this.moviesService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMovieDto: UpdateMovieDto) {
+  update(@Param('id') id: number, @Body() updateMovieDto: UpdateMovieDto) {
     return this.moviesService.update(+id, updateMovieDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: number) {
     return this.moviesService.remove(+id);
   }
 }
