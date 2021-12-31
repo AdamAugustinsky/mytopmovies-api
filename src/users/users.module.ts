c6fb2b2d859e8b94a54ab1a7cb796568849261af
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-12-31 01:17:13 -0300

    fix(usersModule): imports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,8 +3,8 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { Movie } from 'src/movies/entities/movie.entity';
-import { MoviesModule } from 'src/movies/movies.module';
+import { Movie } from '../movies/entities/movie.entity';
+import { MoviesModule } from '../movies/movies.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Movie]), MoviesModule],
