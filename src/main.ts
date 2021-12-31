5c61e329784aa9ee3fe104fdeb1438d8f2b95cc9
AdamAugustinsky <AdamAugustinsky@gmail.com>
2021-12-31 17:24:02 -0300

    refactor: change swagger path to /swagger

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap(): Promise<void> {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document);
 
   app.enableCors();
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
